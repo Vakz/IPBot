cae06998087f95ad3b815ed2555f414817baa917
Fredrik Jonsén <fredrikjonsen@gmail.com>
2016-06-04 16:17:49 +0200

    Functionaly complete

--- src/database.rs
+++ src/database.rs
@@ -7,10 +7,10 @@ use self::sqlite3::{
     DatabaseConnection,
     SqliteResult,
     StatementUpdate,
-    ResultSet,
     ResultRow,
     ResultRowAccess,
-    Query
+    Query,
+    SqliteErrorCode
 };
 use std::error::Error;
 use self::time::Timespec;
@@ -52,33 +52,51 @@ impl Database {
         let mut stmt = self.conn.prepare("INSERT INTO Files (name, dest, user) VALUES ($1, $2, $3)").unwrap();
         match stmt.update(&[&file.name, &file.dest, &file.user]) {
             Ok(i) if i == 0 => Err("Insert failed with unknown reason"),
-            Ok(_) =>Ok(()),
-            Err(err) => Err(err.desc),
+            Ok(_) => Ok(()),
+            Err(err) => {
+                match err.kind {
+                    SqliteErrorCode::SQLITE_CONSTRAINT => Err("A file with that name already exists"),
+                    _ => {
+                        println!("INSERT ERROR: {}", err.to_string());
+                        Err("Insert failed due to unknown error")
+                    }
+                }
+            },
         }
     }
 
     pub fn by_username(&mut self, username: String) -> Option<Vec<DBFile>> {
-        unimplemented!();
+        let mut q = match self.conn.prepare("SELECT * FROM Files WHERE user=$1") {
+            Ok(stmt) => stmt,
+            Err(_) => return None
+        };
+
+        let res = q.query(&[&username], Database::to_file).unwrap().map(|r| r.unwrap());
+
+        return Some(res.collect::<Vec<_>>());
     }
 
-    pub fn get_exact(&mut self, file: String, user: String) -> Option<DBFile> {
-        println!("Search for file: {}, by user: {}", file, user);
-        let to_file = |row: &mut ResultRow| Ok(
+    pub fn get_exact(&mut self, file: String) -> Option<DBFile> {
+        println!("Search for file: {}", file);
+
+        let mut q = match self.conn.prepare("SELECT * FROM Files WHERE name=$1") {
+            Ok(stmt) => stmt,
+            Err(_) => return None
+        };
+
+        return q.query(&[&file], Database::to_file).ok()
+        .and_then(|mut r| r.nth(0))
+        .and_then(|r| r.ok());
+    }
+
+    fn to_file(row: &mut ResultRow) -> SqliteResult<DBFile> {
+        Ok(
             DBFile {
                 name: row.get("name"),
                 dest: row.get("dest"),
                 user: row.get("user"),
                 time: row.get("inserted")
             }
-        );
-
-        let mut q = match self.conn.prepare("SELECT * FROM Files WHERE user=$1 AND name=$2") {
-            Ok(stmt) => stmt,
-            Err(_) => return None
-        };
-
-        return q.query(&[&user, &file], to_file).ok()
-        .and_then(|mut r| r.nth(0))
-        .and_then(|r| r.ok());
+        )
     }
 }
--- src/messagehandler.rs
+++ src/messagehandler.rs
@@ -34,7 +34,7 @@ impl DiscordMessageHandler for FileHandler {
     fn handle_message(&self, message: &str, user: &str, db: &mut Database) -> String {
         println!("FileHandler: Line was  \"{}\"", message);
         lazy_static! {
-            static ref FIND: Regex = Regex::new(r"^!file(?:\s(?P<name>[:word:]+))?(?:\s(?P<dest>[:word:]+))?$").unwrap();
+            static ref FIND: Regex = Regex::new(r"^!file(?:\s([[:word:]ÅÄÖåäö]+))?(?:\s([^\s]+))?$").unwrap();
         }
 
         if let Some(matches) = FIND.captures(&message) {
@@ -42,14 +42,14 @@ impl DiscordMessageHandler for FileHandler {
 
             match m.len() {
                 1 => FileHandler::by_username(user, db),
-
-                2 => FileHandler::by_filename(user, matches.at(1).unwrap(), db),
+                2 => FileHandler::by_filename(matches.at(1).unwrap(), db),
                 3 => FileHandler::insert(user, matches.at(1).unwrap(), matches.at(2).unwrap(), db),
                 _ => "Incorrect usage. Should be: \"!file [filename [destination]]\"".to_string()
             }
         } else {
-            "Should never get here".to_string()
+            "Incorrect usage. Should be: \"!file [filename [destination]]\"".to_string()
         }
+
     }
 }
 
@@ -71,8 +71,8 @@ impl FileHandler {
         }
     }
 
-    fn by_filename(user: &str, filename: &str, db: &mut Database) -> String {
-        db.get_exact(filename.to_string(), user.to_string())
+    fn by_filename(filename: &str, db: &mut Database) -> String {
+        db.get_exact(filename.to_string())
         .as_ref()
         .map_or("You have uploded no file by that name".to_string(), FileHandler::file_to_string)
     }
